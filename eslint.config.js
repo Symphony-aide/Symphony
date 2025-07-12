65510c19566690eeca5f66c5a034f1e43eac3740
alaamer12 <ahmedmuhmmed239@gmail.com>
2025-07-12 05:53:12 +0300

    refactor: update ESLint configuration to use ES module syntax and enhance package.json dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  extends: ["config/eslint.config.js"],
-};
+import baseConfig from 'config/eslint.config.js';
+
+export default [
+  ...baseConfig,
+  {
+    files: [
+      'apps/web/**/*.{ts,tsx,js,jsx}',
+      'frontend/**/*.{ts,tsx,js,jsx}'
+    ],
+    // Additional rules specific to these files
+  }
+];
