c196c4bcf1139103c1d36aebf04e5adbdfbdd48d
alaamer12 <ahmedmuhmmed239@gmail.com>
2025-10-01 12:43:29 +0300

    refactore: remove stale files and packages

--- apps/backend/core_deno/tests/js/statusbar_item_extension.js
+++
@@ -1,5 +0,0 @@
-const item = await Symphony.crateStatusbarItem("test");
-item.onClick(async () => {
-    await item.hide();
-})
-await item.show();
--- apps/backend/sample_extension/main.js
+++
@@ -1,9 +0,0 @@
-const item = await Symphony.crateStatusbarItem("Click me!");
-
-await item.show();
-
-item.onClick(() => {
-    item.setLabel(Math.random().toString())
-})
-
-Symphony.whenUnload().then(() => Symphony.exit())
